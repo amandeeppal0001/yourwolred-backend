3ebdd438dbe42973cb142530d360c4873c86c835
amandeeppal0001 <amandeeppal161@gmail.com>
2025-01-16 18:28:02 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import cookieparser from 'cookie-pasrer';
+// import cookieparser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
 
 const app = express()
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true, limits: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export { app}
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)// uses middelwares
+
+// http://localhost:8000/api/v1/users/register
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 // require('dotenv').config({path: './env})
 import dotenv from "dotenv"
-import mongoose from "mongoose";
-import { DB_NAME} from "./constants.js";
-
-import express from "express"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
     path: './env'
 })
 
 connectDB()
-app.on("error", (error) =>{
-    console.log("ERROR:", err);
-})
+// app.on("error", (error) =>{
+//     console.log("ERROR:", err);
+// })
 .then(() =>{
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`"server is running at port:" ${PORT} `);
-    });
+        console.log(`"server is running at port:" ${process.env.PORT} `);
+    })
 })
-.catch((error) => {
+.catch((err) => {
     console.log("MONGO db connecction failed !!!", err);
 })
 
